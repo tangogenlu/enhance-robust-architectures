c647e07ca9bbfa1fbededd40b969e4ea8c91c921
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2023-12-17 23:20:29 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,58 @@
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+        }
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+
+    }
+        int[] data = new int[10];
+
+}
+        }
+        for (int item : data) {
+        console.log("Random Number: " + item);
+    });
+function generateRandomData() {
+}
+main();
+    const data = [];
+    }
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+    data.forEach(item => {
+}
+    return data;
+function main() {
+
+    for (let i = 0; i < 10; i++) {import random
+
+    main()
+    return data
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
+    data = generate_random_data()
+def main():
+
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":    data.forEach(item => {
+        console.log("Random Number: " + item);
+
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+
+    const data = generateRandomData();
+    const data = [];
+main();
+}
+function generateRandomData() {
+}
+function main() {
+    return data;
+
+    });
