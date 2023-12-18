121c43ac9cdb186db8662b55299e5aac26305e9e
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2023-12-18 19:30:09 +0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,14 +1,75 @@
+    const data = generateRandomData();
+function main() {
+}
+
+main();
+    for (let i = 0; i < 10; i++) {
+
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+    return data;
+        console.log("Random Number: " + item);
+function generateRandomData() {
+    });
+
+}
+    const data = [];def generate_random_data():
+
+        print(f"Random Number: {item}")
+
+def main():
+import random
+if __name__ == "__main__":
+    for item in data:
+    return data
+    main()
 
     data = [random.randint(1, 100) for _ in range(10)]
 
-    main()
-def main():
-    return data
-def generate_random_data():
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-import random
+    data = generate_random_data()    });
 
+    const data = [];
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+    }
+function generateRandomData() {
+
+    const data = generateRandomData();
+    return data;
+}
+main();
+    data.forEach(item => {
+function main() {
+}
+
+        data.push(Math.floor(Math.random() * 100) + 1);}
+
+    data.forEach(item => {
+}
+    const data = generateRandomData();
+
+    const data = [];
+
+    });
+    }
+    for (let i = 0; i < 10; i++) {
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+        console.log("Random Number: " + item);
+main();
+function main() {
+def generate_random_data():
+import random
+        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
 
     for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    data = generate_random_data()
+def main():
+    main()
