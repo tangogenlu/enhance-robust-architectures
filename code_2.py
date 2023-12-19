1520a0bfd9180da4de4a0c8393134be9e15f7a24
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2023-12-19 05:34:58 +0300

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,58 +1,43 @@
-            System.out.println("Random Number: " + item);
-            data[i] = (int) (Math.random() * 100) + 1;
-public class RandomDataGenerator {
-        }
-        for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {
-
-    }
-        int[] data = new int[10];
-
 }
-        }
-        for (int item : data) {
-        console.log("Random Number: " + item);
     });
-function generateRandomData() {
-}
-main();
     const data = [];
-    }
-
-        data.push(Math.floor(Math.random() * 100) + 1);
     const data = generateRandomData();
+
+        console.log("Random Number: " + item);
+    }
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
     data.forEach(item => {
-}
     return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+}
 function main() {
 
-    for (let i = 0; i < 10; i++) {import random
+import random
+def main():
+    for item in data:
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
+def generate_random_data():
+    data = generate_random_data()
+    main()
+    return dataif __name__ == "__main__":
+
+def generate_random_data():
+    for item in data:
+
+def main():
+    data = generate_random_data()
+
 
     main()
     return data
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def generate_random_data():
-    data = generate_random_data()
-def main():
-
-
         print(f"Random Number: {item}")
-if __name__ == "__main__":    data.forEach(item => {
-        console.log("Random Number: " + item);
-
-    for (let i = 0; i < 10; i++) {
-        data.push(Math.floor(Math.random() * 100) + 1);
-    }
-
-    const data = generateRandomData();
-    const data = [];
-main();
-}
-function generateRandomData() {
-}
-function main() {
-    return data;
-
-    });
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
